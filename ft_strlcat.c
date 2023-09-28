0f6e5beb2a8a7838abf24ee9a4a2ddd6ed1f0ed4
Luis Luque <luque.luis@gmail.com>
2023-09-28 21:15:46 +0200

    strlcpy y strlcat refinados

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,41 +6,33 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 01:03:39 by lluque            #+#    #+#             */
-/*   Updated: 2023/09/27 22:12:13 by lluque           ###   ########.fr       */
+/*   Updated: 2023/09/28 21:03:44 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_min(int a, int b)
-{
-	if (a < b)
-		return (a);
-	return (b);
-}
-
-// Remember from man page:
-// 		Note that room for the NUL should be included in dstsize.
-// It seems that size includes the NUL, while length doesn't.
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	int	d;
-	int	s;
-	int	original_dst_len;
+	size_t	d;
+	size_t	s;
+	size_t	original_dst_len;
 
 	d = 0;
-	while (d < (signed)dstsize)
+	while (d < dstsize)
 	{
 		if (dst[d] == '\0')
 			break ;
 		d++;
 	}
-	if (d == (signed)dstsize)
+	if (d == dstsize)
 		return (d + ft_strlen(src));
 	original_dst_len = d;
 	s = 0;
-	while (d < (signed)dstsize - 1)
+	while (d < dstsize - 1)
 	{
+		if (src[s] == '\0')
+			break ;
 		dst[d] = src[s];
 		s++;
 		d++;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 00:20:17 by lluque            #+#    #+#             */
-/*   Updated: 2023/09/26 21:45:59 by lluque           ###   ########.fr       */
+/*   Updated: 2023/09/28 20:23:40 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (i < (signed)dstsize - 1 && src[i] != '\0')
+	while (dstsize != 0 && i < dstsize - 1 && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
